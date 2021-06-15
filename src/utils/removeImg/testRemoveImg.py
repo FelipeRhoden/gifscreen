76a7ba1fbadd099fe1a9abdba3a763c36e940234
FelipeRhoden <FelipeRhoden>
2021-06-14 22:47:22 -0300

    Finalizando tests de remove_img e add logo

--- src/utils/removeImg/testRemoveImg.py
+++ src/utils/removeImg/testRemoveImg.py
@@ -7,14 +7,37 @@ class TestRemove_img(unittest.TestCase):
   # Deveria excluir uma imagem
   def test_remove_img_remove(self):
     img = ImageGrab.grab((0,0,500,500))
-    imgName = "tmp.png"
-    img.save(imgName)
+    path = "tmp.png"
+    img.save(path)
     img.close()
-    pathExists = os.path.exists(imgName)
+    pathExists = os.path.exists(path)
     self.assertTrue(pathExists)
-    remove_img(imgName)
-    pathExists = os.path.exists(imgName)
+    remove_img(path)
+    pathExists = os.path.exists(path)
     self.assertFalse(pathExists)
+
+  #deveria excluir uma imagem em um subdiretório
+  def test_remove_img_subdir(self):
+        dirname = 'img'
+        os.mkdir(dirname)
+        img = ImageGrab.grab((0,0,500,500))
+        path = 'img/img.png'
+        img.save(path)
+        exist = os.path.exists(path)
+        self.assertTrue(exist)
+        remove_img(path)
+        exist = os.path.exists(path)
+        self.assertFalse(exist)    
+        os.rmdir(dirname)
+
+  #deveria retornar um erro ao excluir uma imagem
+  def test_remove_img_except(self):
+        path = 'img/img.png'
+        path2 = 'img.ssd'
+        with self.assertRaises(TypeError):
+            remove_img(path)
+        with self.assertRaises(TypeError):
+            remove_img(path2)
 
 if __name__ == '__main__':
   unittest.main()
