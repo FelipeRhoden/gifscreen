abcb4058c7dd26cf97a4f6b5050d8a9276872214
Felipe Rhoden <53407136+FelipeRhoden@users.noreply.github.com>
2021-06-16 23:35:12 +0000

    começando testes de integração

---
+++ src/testIntegration.py
@@ -0,0 +1,36 @@
+import unittest
+import asyncio
+import time
+from .utils.repeatEngine.repeatEngine import repeat_engine
+from .utils.screenShot.screenShot import screen_shot
+
+class TestIntegretion(unittest.TestCase):
+
+  #deveria salvar 16 frames por segundo durante 2 segundos
+  def test_integration_engine_repeat_print_scree_16fps_2sec(self):
+    coordinates = [(0,0,500,500)]
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(repeat_engine(screen_shot, coordinates, 16, 2))
+    begin = time.time()
+    loop.run_until_complete(results['engine']())
+    end = time.time()
+    listLength = len(results['list'])
+    timeLoop = end - begin
+    self.assertEqual(listLength, 32)
+    self.assertTrue(timeLoop > 1 and timeLoop <= 2.5)
+
+  #deveria salvar 25 frames por segundo durante 7 segundos
+  def test_integration_engine_repeat_print_scree_25fps_7sec(self):
+    coordinates = [(0,0,500,500)]
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(repeat_engine(screen_shot, coordinates, 25, 7))
+    begin = time.time()
+    loop.run_until_complete(results['engine']())
+    end = time.time()
+    listLength = len(results['list'])
+    timeLoop = end - begin
+    self.assertEqual(listLength, 25*7)
+    self.assertTrue(timeLoop > 6 and timeLoop < 7.5)
+
+if __name__ == '__main__':
+    unittest.main()
--- src/utils/repeatEngine/repeatEngine.py
+++ src/utils/repeatEngine/repeatEngine.py
@@ -9,37 +9,42 @@ async def repeat_engine(iteraction, param = None, rps = 1, sec = 1):
       "inLoop": False,
       "list" : [],
     }
+    try:
+      async def taskProcess(iteraction, process_time):
+        try:
+          await asyncio.sleep(process_time)
+          if (type(param) == type([])):
+            results["list"].append(iteraction(*param))
+          else:
+            results["list"].append(iteraction())
+        except:
+          raise TypeError('Erro em uma tarefa do motor de repetição')
 
-    async def taskProcess(iteraction, process_time):
-        await asyncio.sleep(process_time)
-        if (type(param) == type([])):
-          results["list"].append(iteraction(*param))
-        else:
-          results["list"].append(iteraction())
+      for i in range(const_iteraction):
+          tasks.append(
+              asyncio.create_task(
+                  taskProcess(iteraction, process_time)
+              )
+          )
+          process_time += const_time
 
-    for i in range(const_iteraction):
-        tasks.append(
-            asyncio.create_task(
-                taskProcess(iteraction, process_time)
-            )
-        )
-        process_time += const_time
+      loopTask = asyncio.shield(asyncio.gather(*tasks))
 
-    loopTask = asyncio.shield(asyncio.gather(*tasks))
+      async def engine():
+        results['inLoop'] = True
+        try:
+          await loopTask
+        except:
+          None
+        finally:
+          results['inLoop'] = False
 
-    async def engine():
-      results['inLoop'] = True
-      try:
-        await loopTask
-      except:
-        None
-      finally:
-        results['inLoop'] = False
+      def cancel(): 
+        loopTask.cancel()
 
-    def cancel(): 
-      loopTask.cancel()
+      results['engine'] = engine
+      results['cancel'] = cancel
 
-    results['engine'] = engine
-    results['cancel'] = cancel
-
-    return results 
+      return results
+    except:
+      raise TypeError('Erro no motor de repetição') 
