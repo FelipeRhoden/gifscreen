afaa1a70782a41749850e5cfd534940b91361175
FelipeRhoden <FelipeRhoden>
2021-06-15 23:10:12 -0300

    Continuando testes de engine de repetição

--- src/utils/repeatEngine/repeatEngine.py
+++ src/utils/repeatEngine/repeatEngine.py
@@ -2,11 +2,11 @@ import asyncio
 
 async def repeat_engine(iteraction, param = None, rps = 1, sec = 1):
     tasks = []
-    const_time = 1//rps
-    const_iteraction = sec*const_time
+    const_time = 1/rps
+    const_iteraction = rps*sec
     process_time = 0
     results = {
-      "inLoop": True,
+      "inLoop": False,
       "list" : [],
     }
 
@@ -24,7 +24,18 @@ async def repeat_engine(iteraction, param = None, rps = 1, sec = 1):
             )
         )
         process_time += const_time
-    
-    await asyncio.gather(*tasks)
+
+    loopTask = asyncio.gather(*tasks)
+
+    async def engine():
+      results['inLoop'] = True
+      await asyncio.gather(loopTask)
+      results['inLoop'] = False
+
+    def cancel(): 
+      loopTask.cancel()
+
+    results['engine'] = engine
+    results['cancel'] = cancel
 
     return results 
